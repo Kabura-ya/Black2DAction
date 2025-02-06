add3aa774cc57670e9ed73f649b93e8db61c1b4e
DESKTOP-OH0QNN5\kaito <200424kaito@gmail.com>
2025-02-06 23:49:25 +0900

    回避不能技「ギロチン」の追加 巨大蜘蛛の行動をパターン化。 ジャンプ3回＞突進＞ギロチンの繰り返し。 今後更に追加、変更予定。

---
+++ Assets/Enemy/Boss/GiantSpider/Effects/Guillotine/GuillotineAnimationEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuillotineAnimationEvent : MonoBehaviour
+{
+    void Finish()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Enemy/Boss/GiantSpider/GiantSpiderStatus.cs
+++ Assets/Enemy/Boss/GiantSpider/GiantSpiderStatus.cs
@@ -12,7 +12,24 @@ public class GiantSpiderStatus : MonoBehaviour
     [Header("垂直速度"), SerializeField] private float verSpeed = 10;
     public float VerSpeed => verSpeed;//外部取得だけ可能
 
+    [Header("ジャンプ回数"), SerializeField] private int jumpCount = 3;
+    public int JumpCount => jumpCount;
+
+    [Header("突進時間"), SerializeField] private float walkTime = 1.5f;
+    public float WalkTime => walkTime;
+
+    [Header("後隙"), SerializeField] private float coolTime = 0.3f;
+    public float CoolTime => coolTime;
+
     [SerializeField] private Animator anim = null;
+
+    private Transform playerTrans = null;
+    public Transform PlayerTrans => playerTrans;
+
+    void Start()
+    {
+        playerTrans = GameObject.Find("Player").transform;
+    }
 
     public bool IsSpawn()
     {
@@ -28,10 +45,41 @@ public class GiantSpiderStatus : MonoBehaviour
     {
         return anim.GetCurrentAnimatorStateInfo(0).IsName("Walk");
     }
+    public void WalkSwitch(int i)
+    {
+        if(i > 0)
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+    }
 
     public bool IsJump()
     {
         return anim.GetCurrentAnimatorStateInfo(0).IsName("Jump");
+    }
+    public void JumpSwitch(int i)
+    {
+        if (i > 0)
+        {
+            anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+    }
+
+    public bool IsGuillotine()
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).IsName("Guillotine");
+    }
+    public void GuillotineTrigger()
+    {
+        anim.SetTrigger("guillotine");
     }
 
     public bool IsDead()
