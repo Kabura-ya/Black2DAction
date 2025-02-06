1d969015198041ddb37335d15a2d0070469e11f6
shunmiyamoto1-admin <miyashun256@gmail.com>
2025-02-06 17:18:47 +0900

    ボス戦の扉を制作。扉の前でEを押すとボス部屋に入れる。

--- Assets/Stage/Gate/Gate.cs
+++ Assets/Stage/Gate/Gate.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class Gate : MonoBehaviour
 {
     public string sceneName;
+    private bool playerEnter = false;
+    public GameObject guide;//ボタンを押すとボス戦が始まるとかボスのシルエットとか
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,23 @@ public class Gate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerEnter && Input.GetKeyDown(KeyCode.E)) { SceneManager.LoadScene(sceneName); }
     }
 
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player") { SceneManager.LoadScene(sceneName); }
+        if (collision.gameObject.tag == "Player") {
+            playerEnter = true;
+            guide.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player"){
+            playerEnter = false;
+            guide.SetActive(false);
+        }
     }
 }
